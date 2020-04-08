560d25d270c0d79d62dea573106100aa4e3c8fa7
Nora Lashner <nlashner@gmail.com>
2020-04-08 15:28:22 -0400

    original binary search complete

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,8 +2,29 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
-};
+
+	let midIdx= Math.floor(array.length/2)
+	let midpoint = array[midIdx]
+
+	if(array.length === 1){
+		if(array[0] === target){
+			return true
+  	} else return false
+	}
+
+	else {
+		if (target >= midpoint){
+			let splitArr = array.slice(midIdx)
+			return binarySearch(splitArr, target)
+		}
+
+		if(target < midpoint){
+			let splitArr = array.slice(0, midIdx)
+			return binarySearch(splitArr, target)
+		}
+  }
+}
+
 
 /*
 	EXTRA CREDIT:
